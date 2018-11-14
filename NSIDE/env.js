48337cfb4b6ec2b0beb403f149616ec64f60953b
Jamie Birch work laptop <jbizzle5@gmail.com>
2018-11-14 22:39:56 +0000

    TS v2.7.2

--- NSIDE/env.js
+++ NSIDE/env.js
@@ -1,2 +1,2 @@
-// window.localTSVersion = { '2.7.2': { monaco: '0.11.1', lib: '/monaco-typescript/2.7.2' } }
-window.localTSVersion = { '2.9.1': { monaco: '0.13.1', lib: '/monaco-typescript/2.9.1' } }
+window.localTSVersion = { '2.7.2': { monaco: '0.11.1', lib: '/monaco-typescript/2.7.2' } }
+// window.localTSVersion = { '2.9.1': { monaco: '0.13.1', lib: '/monaco-typescript/2.9.1' } }
